38deebc7567cc1b9b64f631deb1f7bf36967567b
ADm1n766 <kiwigruen99@gmx.de>
2025-03-05 09:52:11 +0100

    Close Warning-windows

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,7 +4,7 @@ interface AlertProps {
     onButtonClick: () => void; // Prop for Button-Click-Handler
 }
 
-const Alert = ({onButtonClick}: AlertProps) => {
+const Alert: React.FC<AlertProps> = ({onButtonClick}) => {
     const [showAlert , setShowAlert] = React.useState(false);
     const [message , setMessage] = React.useState('');
 
@@ -16,8 +16,15 @@ const Alert = ({onButtonClick}: AlertProps) => {
     return (
         <div style={{margin: '20px'}}>
             {showAlert && (
-                <div className="alert alert-warning" role="alert">
+                <div className="alert alert-warning alert-dismissible fade show" role="alert">
                     {message}
+                    <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="alert"
+                        aria-label="Close"
+                        onClick={() => setShowAlert(false)}
+                    ></button>
                 </div>
             )}
             <button
